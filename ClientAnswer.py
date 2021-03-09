8b33383d650aafa8d6c0bba4d36b082dee21b85d
Tanya Prasad <55975635+maruyari@users.noreply.github.com>
2021-03-09 15:56:48 +0530

    Update ClientAnswer.py

--- ClientAnswer.py
+++ ClientAnswer.py
@@ -1,7 +1,7 @@
 import socket  # Import socket module
 
 HEADER = 64
-PORT = 5678
+PORT = 8763
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "disconnect"
 SERVER = "127.0.1.1"
@@ -9,23 +9,20 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 answers = {"1": "one", "2": "two", "3": "three"}
-# key=""
+
 
 def send(msg):
+    print("eee" + msg)
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    # key=client.recv(2048).decode(FORMAT)
-    # print(key)
 
 
-# key = "1"
-# send("HELLO WORLD")
-key=client.recv(2048).decode(FORMAT)
-print(key)
-send(answers[key])
-print(answers[key])
-# send(DISCONNECT_MESSAGE)
+while True:
+    key = client.recv(2048).decode(FORMAT)
+    print(key)
+    send(answers[key])
+    print(answers[key])
