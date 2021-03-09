9c1a7d34e9714b7113ec69f41ea1f0d016650d29
Tanya Prasad <55975635+maruyari@users.noreply.github.com>
2021-03-09 15:09:40 +0530

    Create ServeQnA.py

---
+++ ServeQnA.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5678
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "disconnect"
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(ADDR)
+
+
+def handle_client1(conn1, addr1, conn2, addr2):  # recives question from client 1, sends question to client 2.
+    print(f"[NEW CONNECTION]{addr1} connected ")
+    print(f"[NEW CONNECTION]{addr2} connected ")
+    connected = True
+    while connected:
+        msg_length = conn1.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            question = conn1.recv(msg_length).decode(FORMAT)
+            print(f"[{addr1}]{question}")
+            if question == DISCONNECT_MESSAGE:
+                break
+            conn2.send(question.encode(FORMAT))
+            answer_length = conn2.recv(HEADER).decode(FORMAT)
+
+            if answer_length:
+                answer_length = int(msg_length)
+                answer = conn2.recv(msg_length).decode(FORMAT)
+                print(f"[{addr2}]{answer}")
+                if question == DISCONNECT_MESSAGE:
+                    break
+                conn1.send(answer.encode(FORMAT))
+
+    conn1.close()
+    conn2.close()
+
+
+
+def start():
+    server.listen()
+    print(f"server is listening on {SERVER}")
+    # while True:
+    #     conn, addr = server.accept()
+    #     thread = threading.Thread(target=handle_client1, args=(conn, addr))
+    #     thread.start()
+    #     print(f"[ACTIVE CONNECTIONS]{threading.activeCount() - 1}")
+    conn1, addr1 = server.accept()  # receives quesion
+    conn2, addr2 = server.accept()  # send answer to this
+    # handle_client1(conn1, addr1, conn2, addr2)
+    thread = threading.Thread(target=handle_client1, args=(conn1, addr1, conn2, addr2))
+    thread.start()
+    print(f"[ACTIVE CONNECTIONS]{threading.activeCount() - 1}")
+
+print("[STARTING] server is starting....")
+start()
